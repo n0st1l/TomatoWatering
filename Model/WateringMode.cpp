81aa2f1d313ea9d157d34cd90c549b90f585bf69
Lukas Köpfli <n0_st1l@hotmail.com>
2015-11-02 22:13:25 +0100

    Add WateringMode Class

--- Model/WateringMode.cpp
+++ Model/WateringMode.cpp
@@ -8,11 +8,147 @@
 #include "WateringMode.h"
 
 WateringMode::WateringMode() {
-	// TODO Auto-generated constructor stub
 
+	for(int i = 0 ; i < NUMBEROFPOTS ; i++)
+	{
+		potArray[i] = new PotModel();
+	}
+	for(int i = 0 ; i < NUMBEROFWATERINGSETTINGS ; i++)
+	{
+		wateringSettingsArray[i] = new WateringSettings();
+	}
+	wateringModeState = new WateringModeState();
 }
 
 WateringMode::~WateringMode() {
-	// TODO Auto-generated destructor stub
+
+	for(int i = 0 ; i < NUMBEROFPOTS ; i++)
+	{
+		delete potArray[i];
+	}
+	for(int i = 0 ; i < NUMBEROFWATERINGSETTINGS ; i++)
+	{
+		delete wateringSettingsArray[i];
+	}
+	delete wateringModeState;
 }
 
+PotModel* WateringMode::getPot(int index) {
+
+	if(HelperClass::Instance()->isIntInRange(index, 0, ARRAY_END(NUMBEROFPOTS) ) == true)
+	{
+		return potArray[index];
+	}
+	else
+	{
+		return new PotModel();
+	}
+}
+
+WateringSettings* WateringMode::getWateringSettings(int index) {
+
+	if(HelperClass::Instance()->isIntInRange(index, 0, ARRAY_END(NUMBEROFWATERINGSETTINGS) ) == true)
+	{
+		return wateringSettingsArray[index];
+	}
+	else
+	{
+		return new WateringSettings();
+	}
+}
+
+bool WateringMode::addPot(PotModel* newPot) {
+
+	if( potAlreadyExists(newPot) == false )
+	{
+		potArray[newPot->getPotIndex()] = newPot;
+		return true;
+	}
+
+	return false;
+}
+
+bool WateringMode::removePot(int potIndexToRemove) {
+
+	if(HelperClass::Instance()->isIntInRange(potIndexToRemove, 0, ARRAY_END(NUMBEROFPOTS) ) == true)
+	{
+		delete potArray[potIndexToRemove];
+		potArray[potIndexToRemove] = new PotModel();
+
+		for(int i = 0 ; i < NUMBEROFWATERINGSETTINGS ; i++)
+		{
+			if(wateringSettingsArray[i]->getPotIndex() == potIndexToRemove)
+			{
+				this->removeWateringSettings(i);
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+bool WateringMode::addWateringSettings(WateringSettings* newWateringSettings) {
+
+	if( wateringSettingsAlreadyExists(newWateringSettings) == false )
+	{
+		wateringSettingsArray[newWateringSettings->getWateringSettingsIndex()] = newWateringSettings;
+		return true;
+	}
+
+	return false;
+}
+
+bool WateringMode::removeWateringSettings(int wateringSettingsIndexToRemove) {
+
+	if(HelperClass::Instance()->isIntInRange(wateringSettingsIndexToRemove, 0, ARRAY_END(NUMBEROFWATERINGSETTINGS) ) == true)
+	{
+		delete wateringSettingsArray[wateringSettingsIndexToRemove];
+		wateringSettingsArray[wateringSettingsIndexToRemove] = new WateringSettings();
+
+		return true;
+	}
+
+	return false;
+}
+
+int WateringMode::getFreeWateringSettingsIndex() {
+
+	for(int i = 0 ; i < NUMBEROFWATERINGSETTINGS ; i++)
+	{
+		if(wateringSettingsArray[i]->getWateringSettingsIndex() == -1)
+		{
+			return i;
+		}
+	}
+
+	return NUMBEROFWATERINGSETTINGS;
+}
+
+bool WateringMode::potAlreadyExists(PotModel* potToCheck) {
+
+	for(int i = 0 ; i < NUMBEROFPOTS ; i++)
+	{
+		if( (*potArray[i]) == (*potToCheck) )
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool WateringMode::wateringSettingsAlreadyExists(
+		WateringSettings* wateringSettingsToCheck) {
+
+	for(int i = 0 ; i < NUMBEROFWATERINGSETTINGS ; i++)
+	{
+		if( (*wateringSettingsArray[i]) == (*wateringSettingsToCheck) )
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
--- Model/WateringMode.h
+++ Model/WateringMode.h
@@ -8,10 +8,43 @@
 #ifndef MODEL_WATERINGMODE_H_
 #define MODEL_WATERINGMODE_H_
 
+#include "..\Help\HelperClass.h"
+#include "PotModel.h"
+#include "WateringSettings.h"
+#include "WateringModeState.h"
+
+#define NUMBEROFPOTS				4
+#define NUMBEROFWATERINGSETTINGS	32
+
 class WateringMode {
 public:
 	WateringMode();
 	virtual ~WateringMode();
+
+	PotModel* getPot(int index);
+
+	WateringSettings* getWateringSettings(int index);
+
+	bool addPot(PotModel* newPot);
+	bool removePot(int potIndexToRemove);
+
+	bool addWateringSettings(WateringSettings* newWateringSettings);
+	bool removeWateringSettings(int wateringSettingsIndexToRemove);
+
+	int getFreeWateringSettingsIndex();
+
+	inline WateringModeState* getWateringModeState() { return wateringModeState; }
+
+
+private:
+	PotModel* potArray[NUMBEROFPOTS];
+	WateringSettings* wateringSettingsArray[NUMBEROFWATERINGSETTINGS];
+	WateringModeState* wateringModeState;
+
+	bool potAlreadyExists(PotModel* potToCheck);
+	bool wateringSettingsAlreadyExists(WateringSettings* wateringSettingsToCheck);
+
+
 };
 
 #endif /* MODEL_WATERINGMODE_H_ */
