84774acdb68240772d1f3f92944c430f5606078b
Lukas Köpfli <n0_st1l@hotmail.com>
2015-11-02 22:13:06 +0100

    Add WateringModeState class

--- Model/WateringModeState.cpp
+++ Model/WateringModeState.cpp
@@ -8,11 +8,29 @@
 #include "WateringModeState.h"
 
 WateringModeState::WateringModeState() {
-	// TODO Auto-generated constructor stub
-
+	this->isWatering = false;
+	this->isAutomaticMode = false;
+	this->isManualMode = false;
+	this->actualWateringSettingsIndex = -1;
 }
 
 WateringModeState::~WateringModeState() {
 	// TODO Auto-generated destructor stub
 }
 
+void WateringModeState::setIsWatering(bool isWatering) {
+	this->isWatering = isWatering;
+}
+
+void WateringModeState::setIsAutomaticMode(bool isAutomaticMode) {
+	this->isAutomaticMode = isAutomaticMode;
+}
+
+void WateringModeState::setIsManualMode(bool isManualMode) {
+	this->isManualMode = isManualMode;
+}
+
+void WateringModeState::setActualWateringSettingsIndex(
+		int actualWateringSettingsIndex) {
+	this->actualWateringSettingsIndex = actualWateringSettingsIndex;
+}
--- Model/WateringModeState.h
+++ Model/WateringModeState.h
@@ -12,6 +12,27 @@ class WateringModeState {
 public:
 	WateringModeState();
 	virtual ~WateringModeState();
+
+	inline bool getIsWatering() { return isWatering; }
+	void setIsWatering(bool isWatering);
+
+	inline bool getIsAutomaticMode() { return isAutomaticMode; }
+	void setIsAutomaticMode(bool isAutomaticMode);
+
+	inline bool getIsManualMode() { return isManualMode; }
+	void setIsManualMode(bool isManualMode);
+
+	inline int getActualWateringSettingsIndex() { return actualWateringSettingsIndex; }
+	void setActualWateringSettingsIndex(int actualWateringSettingsIndex);
+
+
+private:
+	bool isWatering;
+	bool isAutomaticMode;
+	bool isManualMode;
+	int actualWateringSettingsIndex;
+
+
 };
 
 #endif /* MODEL_WATERINGMODESTATE_H_ */
