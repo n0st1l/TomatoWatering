fda134a665101f8f7eb3e26d518dc2639d4c6b25
Lukas Köpfli <n0_st1l@hotmail.com>
2015-11-02 22:12:44 +0100

    Add tests

--- TomatoWatering.h
+++ TomatoWatering.h
@@ -7,11 +7,34 @@
 #ifndef _TomatoWatering_H_
 #define _TomatoWatering_H_
 #include "Arduino.h"
-//add your includes for the project TomatoWatering here
+
+// NOTE add your includes for the project TomatoWatering here
+
+/*Include Libraries*/
 #include "TimerOne.h"
-#include "Model/WateringSettings.h"
+#include "date.h"
+#include "DHT.h"
+#include "DS1302.h"
+#include "LiquidCrystal.h"
+#include "time.h"
+
+/*Include Helpers*/
+#include "Help/HelperClass.h"
+#include "Help/hwPins.h"
+#include "Help/types.h"
+
+/*Include Models*/
+#include "Model/OperatingState.h"
 #include "Model/PotModel.h"
+#include "Model/WateringMode.h"
+#include "Model/WateringModeState.h"
+#include "Model/WateringSettings.h"
+
+/*Include Controls*/
+#include "Control/OperatingControl.h"
+
 //end of add your includes here
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -24,7 +47,6 @@ void setup();
 //add your function definitions for the project TomatoWatering here
 void print_time();
 void printHelloWorld();
-PotModel* tomatoPot;
 
 
 //Do not add code below this line
