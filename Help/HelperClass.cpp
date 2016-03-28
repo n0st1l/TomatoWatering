7b12c98fa6f4f16775fb497564d00854b103581e
Lukas Köpfli <n0_st1l@hotmail.com>
2016-03-28 20:21:22 +0200

    Comment the debug messages

--- Help/HelperClass.cpp
+++ Help/HelperClass.cpp
@@ -50,24 +50,24 @@ bool HelperClass::isFloatInRange(float value, float min, float max) {
 bool HelperClass::getGainAndOffset(float x1, float x2, float y1, float y2,
 		float* gain, float* offset) {
 
-	String debugMsg = "getGainAndOffset(float " + String(x1) +
-			", float " + String(x2) +
-			", float " + String(y1) +
-			", float " + String(y2) + ")";
-	LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
+	//	String debugMsg = "getGainAndOffset(float " + String(x1) +
+	//			", float " + String(x2) +
+	//			", float " + String(y1) +
+	//			", float " + String(y2) + ")";
+	//	LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
 
 	if( (x1 >= x2) || (y1 >= y2) ) {
 
-		debugMsg = "min and max mismatch";
-		LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
+		//		debugMsg = "min and max mismatch";
+		//		LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
 
 		return false;
 	}
 	*gain = (y2 - y1) / (x2 - x1);
 	*offset = y1 - (*gain * x1);
 
-	debugMsg = "gain " + String(*gain) + ", offset " + String(*offset);
-	LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
+	//	debugMsg = "gain " + String(*gain) + ", offset " + String(*offset);
+	//	LOG_DAEMON_DEBUG(eHelperClass, debugMsg);
 
 	return true;
 }
